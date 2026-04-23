9f4e7ef940c36df4661aa510a98341a6a60a0143
Sebastian M <sebasmendez2409@gmail.com>
2026-04-22 22:24:32 -0500

    Conectando el front con el back

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 from backend.api.endpoints import router
 
 
 app = FastAPI(
     title="API Habi",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Esto permite que cualquier origen acceda
+    allow_credentials=True,
+    allow_methods=["*"], # Permite GET, POST, etc.
+    allow_headers=["*"], # Permite todos los encabezados
 )
 
 app.include_router(
--- frontend/script.js
+++ frontend/script.js
@@ -3,22 +3,25 @@ async function buscar() {
     const ano = document.getElementById('filtro-ano').value;
     const estado = document.getElementById('filtro-estado').value;
 
+    
+
     // Construimos la URL con los parámetros
     let url = `http://127.0.0.1:8000/api/properties?`;
-    if(ciudad) url += `ciudad=${ciudad}&`;
-    if(ano) url += `ano=${ano}&`;
-    if(estado) url += `estado=${estado}&`;
+    if(ciudad) url += `city=${ciudad}&`;
+    if(ano) url += `year=${ano}&`;
+    if(estado) url += `status=${estado}&`;
 
     try {
         const response = await fetch(url);
         const datos = await response.json();
-        renderizar(datos);
+        console.log("DATOS RECIBIDOS DEL BACKEND:", datos);
+        render(datos);
     } catch (error) {
         console.error("Error al conectar con la API:", error);
     }
 }
 
-function renderizar(propiedades) {
+function render(propiedades) {
     const contenedor = document.getElementById('contenedor-propiedades');
     contenedor.innerHTML = ""; // Limpiar
 
@@ -27,15 +30,15 @@ function renderizar(propiedades) {
             <div class="col">
                 <div class="card h-100 shadow-sm">
                     <div class="card-body">
-                        <h5 class="card-title text-primary">${p.direccion}</h5>
+                        <h5 class="card-title text-primary">${p.address}</h5>
                         <p class="card-text">
-                            <strong>Ciudad:</strong> ${p.ciudad}<br>
-                            <strong>Año:</strong> ${p.ano_construccion}<br>
-                            <strong>Precio:</strong> $${p.precio_venta.toLocaleString()}
+                            <strong>Ciudad:</strong> ${p.city}<br>
+                            <strong>Año:</strong> ${p.year}<br>
+                            <strong>Precio:</strong> $${(p.price || p.precio || 0).toLocaleString('es-CO')}
                         </p>
                     </div>
                     <div class="card-footer bg-white border-0">
-                        <span class="badge bg-info text-dark">${p.estado_actual}</span>
+                        <span class="badge bg-info text-dark">${p.status}</span>
                     </div>
                 </div>
             </div>
